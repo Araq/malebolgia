bf2df6f0e4f13843da6afd5bb59eea2fc2c87ede
Araq <rumpf_a@web.de>
2023-05-15 07:54:13 +0200

    better example

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,17 +1,18 @@
 
 import malebolgia
+import std / isolation
 
 proc foo =
   echo "foo"
 
-proc bar =
-  echo "bar"
+proc bar(s: string) =
+  echo "bar ", s
 
 proc main =
   var m = createMaster()
   m.awaitAll:
     m.spawn foo()
     for i in 0..<1000:
-      m.spawn bar()
+      m.spawn bar($i)
 
 main()
