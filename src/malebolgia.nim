a887621306f781d380f06d84ed14dfe2b9fa66b7
Antonis Geralis <43617260+planetis-m@users.noreply.github.com>
2023-05-16 13:06:26 +0300

    Update malebolgia.nim

--- src/malebolgia.nim
+++ src/malebolgia.nim
@@ -81,8 +81,8 @@ proc send(item: sink PoolTask) =
     inc chan.count
     chan.data[chan.tail] = item
     chan.tail = (chan.tail + 1) and FixedChanMask
-    release(chan.L)
     signal(chan.dataAvailable)
+    release(chan.L)
   else:
     release(chan.L)
     quit "logic bug: queue not empty after signal!"
@@ -98,8 +98,8 @@ proc worker() {.thread.} =
       dec chan.count
       item = move chan.data[chan.head]
       chan.head = (chan.head + 1) and FixedChanMask
-    release(chan.L)
     signal(chan.spaceAvailable)
+    release(chan.L)
     if not item.m.stopToken.load(moRelaxed):
       try:
         atomicInc busyThreads
