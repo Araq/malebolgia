8076b67063e5f0e35de5899bf28bcdabf10a0e4a
Araq <rumpf_a@web.de>
2023-05-17 11:28:59 +0200

    added web crawler example

---
+++ tests/tcrawer.nim
@@ -0,0 +1,105 @@
+discard """
+  action: "compile"
+"""
+
+# Simple web crawler that uses `spawn` for parallel downloads and parsing.
+
+import malebolgia
+import malebolgia / ticketlocks
+
+import std / [isolation, httpclient, os, streams, parsexml, strutils, sets]
+
+const
+  StartUrl = "https://nim-lang.org"
+
+proc combine(url, base: string): string =
+  if url.endsWith(".tar.xz") or url.endsWith(".zip"):
+    result = ""
+  elif url.startsWith(base):
+    result = url
+  elif url.startsWith("/"):
+    result = base & url
+  elif not url.startsWith("http"):
+    result = base & "/" & url
+  else:
+    result = ""
+
+var
+  seen: HashSet[string]
+  L: TicketLock
+
+proc containsOrIncl(s: string): bool =
+  {.gcsafe.}:
+    acquire L
+    result = containsOrIncl(seen, s)
+    release L
+
+proc download(url: string; master: ptr Master) {.gcsafe.}
+
+proc `=?=` (a, b: string): bool =
+  return cmpIgnoreCase(a, b) == 0
+
+proc extractLinks(html: string; master: ptr Master) {.gcsafe.} =
+  var links = 0 # count the number of links
+  var s = newStringStream(html)
+  var x: XmlParser
+  open(x, s, "<content.html>")
+  next(x) # get first event
+  block mainLoop:
+    while true:
+      case x.kind
+      of xmlElementOpen:
+        # the <a href = "xyz"> tag we are interested in always has an attribute,
+        # thus we search for ``xmlElementOpen`` and not for ``xmlElementStart``
+        if x.elementName =?= "a":
+          x.next()
+          if x.kind == xmlAttribute:
+            if x.attrKey =?= "href":
+              var link = x.attrValue
+              inc(links)
+              # skip until we have an ``xmlElementClose`` event
+              while true:
+                x.next()
+                case x.kind
+                of xmlEof: break mainLoop
+                of xmlElementClose: break
+                else: discard
+              x.next() # skip ``xmlElementClose``
+              # now we have the description for the ``a`` element
+              var desc = ""
+              while x.kind == xmlCharData:
+                desc.add(x.charData)
+                x.next()
+              link = combine(link, StartUrl)
+              if link.len > 0 and not containsOrIncl(link):
+                master[].spawn download(link, master)
+        else:
+          x.next()
+      of xmlEof: break # end of file reached
+      of xmlError:
+        #echo(errorMsg(x))
+        x.next()
+      else: x.next() # skip other events
+
+  echo($links & " link(s) found!")
+  x.close()
+
+
+proc download(url: string; master: ptr Master) =
+  echo "downloading ", url
+  var client = newHttpClient()
+  var content = ""
+  try:
+    content = client.getContent(url)
+  except HttpRequestError:
+    discard
+  finally:
+    client.close()
+  if content.len > 0:
+    master[].spawn extractLinks(content, master)
+
+seen.incl StartUrl
+
+var master = createMaster()
+master.awaitAll:
+  master.spawn download(StartUrl, addr master)
